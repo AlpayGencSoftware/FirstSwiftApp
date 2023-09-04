224cfe7a85e4f4b8259d1a599eb68dc3efb9c49f
AlpayGencSoftware <gencoalpay@gmail.com>
2023-09-04 16:32:43 +0300

    Button and icons added

--- FirstSwiftUI/ContentView.swift
+++ FirstSwiftUI/ContentView.swift
@@ -10,14 +10,47 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .center) {
-            Image("metallicaLogo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Image("metallica")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+            VStack(alignment: .center){
+                Image("metallicaLogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image("metallica")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            HStack(alignment: .center){
+                Text("Kirk").padding()
+                Text("Lars").padding()
+                Text("Rob").padding()
+                Text("James").padding()
+            }
+            
+            Button(action: {
+                //code
+            }){
+                Text("Start")
+            }.padding(.top, 20.0)
+            HStack(alignment: .center){
+                Image(systemName: "minus")
+                    .padding([.top, .leading], 20.0)
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Image(systemName: "plus")
+                    .padding([.top, .leading], 20.0)
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Image(systemName: "pencil")
+                    .padding([.top, .leading], 20.0)
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Image(systemName: "trash")
+                    .padding([.top, .leading], 20.0)
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+            }
         }
         .padding()
+       
     }
 }
 
