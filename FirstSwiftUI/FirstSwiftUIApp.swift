04847490708e0a3d7c24bf9c44aa07ff1fe3d3d3
AlpayGencSoftware <gencoalpay@gmail.com>
2023-09-04 12:22:08 +0300

    Initial Commit

---
+++ FirstSwiftUI/FirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIApp.swift
+//  FirstSwiftUI
+//
+//  Created by AlpayGenc on 4.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
